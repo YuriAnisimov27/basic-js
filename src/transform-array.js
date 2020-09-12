0b5e6931420ffd6ceb2769759e2e261b11b3ae63
YuriAnisimov27 <yurianisimov27@gmail.com>
2020-09-13 02:52:09 +0300

    feat: add solution for transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
+module.exports = function transform(arr) {
+  if (Array.isArray(arr)) {
+    const res = [...arr];
+    for (let i = 0; i < res.length; i++) {
+      switch (res[i]) {
+        case '--discard-next':
+          res[i + 1] = undefined;
+          res[i] = undefined;
+          break;
+        case '--discard-prev':
+          if (i === 0) {
+            res[i] = undefined;
+          } else {
+            res[i - 1] = undefined;
+            res[i] = undefined;
+          }
+          break;
+        case '--double-next':
+          if ((i + 1) === res.length) {
+            res[i] = undefined;
+          } else {
+            res[i] = res[i + 1];
+          }
+          break;
+        case '--double-prev':
+          res[i] = res[i - 1];
+          break;
+      }
+    }
+    return res.filter(el => (el !== undefined));
+} else {
+    throw new Error();
+}
   throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
