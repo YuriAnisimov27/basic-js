3e393cd64baeef5e7bdd2203b52a740013b71ee5
YuriAnisimov27 <yurianisimov27@gmail.com>
2020-09-20 23:29:51 +0300

    feat: add solution vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(value = true) {
+    this.direction = value;
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error("error");
+    }
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let index = 0;
+    const res = [...message].map(char => {
+      return (char.charCodeAt(0) < 65 || char.charCodeAt(0) > 90)
+        ? char
+        : String.fromCharCode((char.charCodeAt(0) + key.charCodeAt(index++ % key.length)) % 26 + 65);
+    });
+    return this.direction ? res.join('') : [...res].reverse().join('');
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error("error");
+    }
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let index = 0;
+    const res = [...message].map(char => {
+      return (char.charCodeAt(0) < 65 || char.charCodeAt(0) > 90)
+        ? char
+        : String.fromCharCode((char.charCodeAt(0) - key.charCodeAt(index++ % key.length) + 26) % 26 + 65);
+    });
+    return this.direction ? res.join('') : [...res].reverse().join('');
   }
 }
 
